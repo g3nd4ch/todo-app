954598225af51b81ec28ea21a1ad4f61144b82f6
Andrew <andrew@example.com>
2026-06-07 11:39:10 +0300

    feat:setup http middleware, custom response writer and core domain models

---
+++ internal/features/users/transport/http/transport.go
@@ -0,0 +1,15 @@
+package users_transport_http
+
+type UsersHTTPHandler struct{
+	usersService UsersService
+}
+
+type UsersService interface{
+
+}
+
+func NewUsersHTTPHandler(usersService UsersService) *UsersHTTPHandler{
+	return &UsersHTTPHandler{
+		usersService: usersService,
+	}
+}
